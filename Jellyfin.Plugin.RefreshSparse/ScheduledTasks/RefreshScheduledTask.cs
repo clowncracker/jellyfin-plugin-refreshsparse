ac10b399ef17c4a52458bdb2356e5cca22e55601
clowncracker <49218426+clowncracker@users.noreply.github.com>
2025-10-22 09:55:50 -0700

    Update RefreshScheduledTask.cs

--- Jellyfin.Plugin.RefreshSparse/ScheduledTasks/RefreshScheduledTask.cs
+++ Jellyfin.Plugin.RefreshSparse/ScheduledTasks/RefreshScheduledTask.cs
@@ -61,8 +61,8 @@ namespace Jellyfin.Plugin.RefreshSparse
                     MinDateCreated = minDate,
                     OrderBy = new[]
                         {
-                            (ItemSortBy.SeriesSortName, SortOrderDirection.Ascending),
-                            (ItemSortBy.SortName, SortOrderDirection.Ascending)
+                            (ItemSortBy.SeriesSortName, (SortOrder?)null),
+                            (ItemSortBy.SortName, (SortOrder?)null)
                         }
                 }).Cast<Episode>().Where(i => (maxDays == -1 || i.PremiereDate >= minDate || !i.PremiereDate.HasValue)
                     && MinutesSinceRefresh(i) > PluginConfig.RefreshCooldownMinutes
